0a2a3a742699d2c60e7fa3e531d2aee49d696ac5
elmiramm <elmira.m.m@yandex.ru>
2023-04-07 19:59:06 +0300

    intro.scss, intro-slider-html,scss

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 import * as flsFunctions from "./modules/functions.js";
-import Swiper, { Navigation, Pagination, Parallax } from 'swiper';
+import Swiper, { Navigation, Pagination, Parallax, Autoplay } from 'swiper';
 import mixitup from 'mixitup';
 
 flsFunctions.isWebp();
@@ -16,56 +16,55 @@ flsFunctions.isWebp();
 
 
 // ------Swiper-------
-Swiper.use([Navigation, Pagination, Parallax]);
+Swiper.use([Navigation, Pagination, Parallax, Autoplay]);
 
-if (document.querySelector('.slider-contact__body')) {
-	new Swiper('.slider-contact__body', {
-		observer: true,
-		observeParents: true,
-		slidesPerView: 10,
-		slidesPerGroup: 4, //если нужно чтобы кол-во dots было меньше кол-ва slides
-		spaceBetween: 18,
+if (document.querySelector('.slider-intro__body')) {
+	new Swiper('.slider-intro__body', {
+		observer: true, //обновляет swiper при изменении элементов слайдера
+		observeParents: true, //обновляет swiper при изменении в родительских элементах слайдера
+		slidesPerView: 1,
+		spaceBetween: 0,
+		// slidesPerGroup: 4, //если нужно чтобы кол-во dots было меньше кол-ва slides. Slides будут перелистываться группами
+		// centeredSlides: true, //Активный слайд по центру
+		loop: true, // pза последним слайдем будет идти заново первый
 		watchOverflow: false,
 		speed: 800,
-		loop: true,
 		loopAdditionalSlides: 5,
 		preloadImage: false,
 		// parallax: true,
+		simulateTouch: false, //вкл/выкл перетаскинавия на ПК
 		pagination: {
-			el: '.slider-contact__controls .dots',
-			clickable: true,
-			dynamicBullets: true,
+			el: '.slider-intro__controls .dots',
+			clickable: true, //true - можно кликнуть на dotts и перейти на нужный слайд
+			dynamicBullets: false, // ]true - dots будут менять размер в зависимости от того насколько далеко расположены от активного dots
 		},
+		autoplay: {
+			delay: 3000, //Пауза м.д. прокрутками
+			stopOnLastSlide: false,
+			disableOnInteraction: false,
+		},
+		effect: 'fade', //значение по умолчанию - перелистывает слайды, 'fade' - смена прозрачности
 		// navigation: {
 		// 	nextEl: '.slider-section .slider-arrow--next',
 		// 	prevEl: '.slider-section .slider-arrow--prev',
 		// },
-		breakpoints: {
-			0: {
-				slidesPerView: 1.5,
-				spaceBetween: 0,
-			},
-			450: {
-				slidesPerView: 2.5,
-				spaceBetween: 0,
-			},
-			768: {
-				slidesPerView: 3.5,
-				spaceBetween: 0,
-			},
-			992: {
-				slidesPerView: 5,
-				spaceBetween: 15,
-			},
-			1200: {
-				slidesPerView: 6
-			},
-			1750: {
-				slidesPerView: 8
-			},
-		}
+		// breakpoints: { //Mobile First - значения от заданной точки и больше 
+		// 	279: {
+		// 		slidesPerView: 1.1,
+		// 		spaceBetween: 15,
+		// 	},
+		// 	768: {
+		// 		slidesPerView: 2,
+		// 		spaceBetween: 20,
+		// 	},
+		// 	992: {
+		// 		slidesPerView: 3,
+		// 		spaceBetween: 32,
+		// 	},
+		// }
 	});
 }
+
 
 // ------Swiper-End-------
 
